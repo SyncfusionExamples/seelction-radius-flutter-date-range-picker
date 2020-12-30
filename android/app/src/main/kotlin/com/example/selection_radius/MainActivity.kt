8b627ab1f5aad5b3e8e23a37c99236c1249bdd01
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-30 16:03:56 +0530

    FLUT-3311 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/selection_radius/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.selection_radius
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(SelectionRadius());
+
+class SelectionRadius extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child:
+          Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              selectionMode: DateRangePickerSelectionMode.multiple,
+              selectionRadius: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
